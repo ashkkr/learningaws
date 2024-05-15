85ead67be5caf3cb14d2bbdabde457f4ca49d377
ashutosh <ashutoshsangra@gmail.com>
2024-05-15 14:40:20 +0530

    made change to text added yaml

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-app.get("/",(req,res)=>{res.json({"message" : "Hey this is me testing env"});})
+app.get("/",(req,res)=>{res.json({"message" : "Hey this is me with yaml"});})
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
