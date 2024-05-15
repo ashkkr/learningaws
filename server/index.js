5e660f9c7234eb3eaa7a887052e8104420452de2
ashutosh <ashutoshsangra@gmail.com>
2024-05-15 13:09:56 +0530

    updateed indexx

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-app.get("/",(req,res)=>{res.json({"message" : "Hey this is me"});}
+app.get("/",(req,res)=>{res.json({"message" : "Hey this is me updated"});}
 
 // Connect to MongoDB
 // DONT MISUSE THIS THANKYOU!!
